52662b3969fc183704fe8a7b6d5902eb95eec94b
Sofia <sofigava@gmail.com>
2021-03-23 21:05:39 -0500

    saludame 2 correcion

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ String.prototype.capitalize = function() {
 app.get('/:name', (req, res) => {
 
   var name=req.params.name || 'desconocido';
- ;
   res.send(`<h1>Hola ${name.capitalize()} !</h1>`);
   
 });
