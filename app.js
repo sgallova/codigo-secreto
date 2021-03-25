c51247d129c9eaa016632a064d4dc5c58c8f9260
Sofia <sofigava@gmail.com>
2021-03-25 18:20:26 -0500

    ejercicio saludame2

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-
 app.get('/:name', (req, res) => {
-
-  res.send(`<h1>Hola ${req.params.capitalize() || 'desconocido'} !</h1>`);
-  
+  let nombre = req.params.name;
+  res.send(`<h1>Hola ${nombre.capitalize()}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
